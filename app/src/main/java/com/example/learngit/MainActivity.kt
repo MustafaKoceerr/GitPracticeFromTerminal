442ffe309822b7e05c249648e0e109387d8565f8
MustafaKoceerr <mustafakoceerr@hotmail.com>
2024-07-24 00:49:35 +0300

    initial commit with love

---
+++ app/src/main/java/com/example/learngit/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.learngit
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        Toast.makeText(this@MainActivity, "Hello World", Toast.LENGTH_LONG).show()
+
+        println("Change 3 ")
+        println("Change 4")
+        println("Stash changes here")
+
+        println("Musti is dev branch here")
+
+        println("This line will merge")
+
+    }
+}
